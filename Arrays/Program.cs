fc4768f7e4416eee8bd7b081992879232f2552c1
calit <calit@LAPTOP-QDSNDEMJ>
2022-07-07 15:20:14 +0300

    Добавьте файлы проекта.

---
+++ Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = GetRandom(5, -1, 8);
+            Write(array);
+            GetBubble(array);
+            Write(array);
+        }
+        static int[] GetRandom(int size, int min, int max)
+        {
+            Random rnd = new Random();
+            int[] rndArray = new int[size];
+            for (int i = 0; i < rndArray.Length; i++)
+            {
+                rndArray[i] = rnd.Next(min, max);
+            }
+            return rndArray;
+        }
+        static void Write(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+        static void GetBubble(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int numb = array[i];
+                        array[i] = array[j];
+                        array[j] = numb;
+                    }
+                }
+            }
+        }
+    }
+}
